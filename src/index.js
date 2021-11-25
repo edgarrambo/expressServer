41586c1b2083d1b0932332382f7d1904a2efdda7
Edgar Alonzo <ragde956@gmail.com>
2021-11-25 00:08:16 -0600

    error handler added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+
 import config from './utils/config'
 import logger from './utils/logger'
+import errors from './utils/errors'
 
 const app = express()
 
 app.use(logger.middleware)
+app.use(helmet())
+app.use(
+  cors({
+    origin: config.origin,
+  }),
+)
 
 app.get('/', (req, res) => {
   logger.log.success('Calling Root')
   res.send({ msg: 'Hello There' })
 })
+
+app.use(errors.notFound)
+app.use(errors.errorHandler)
 
 app.listen(config.port)
